b4ee17eecec36dc3b545e34f184a8fd31853b9fb
Ihor Sharov <cookie50ds@gmail.com>
2023-11-27 21:58:05 +0200

    add crops consts

---
+++ src/components/const/crops/crops.js
@@ -0,0 +1,13 @@
+export const WHEAT = {
+  fieldName: 'wheat',
+  count: 1,
+  time: 2,
+}
+
+export const CARROT = {
+  fieldName: 'carrot',
+  count: 1,
+  time: 4,
+}
+
+export const list = [WHEAT, CARROT]
