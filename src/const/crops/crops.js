e1855b35a253c0dda6cbed3c168432222b5e7735
Ihor Sharov <cookie50ds@gmail.com>
2023-11-29 00:33:27 +0200

    add crop images

--- src/const/crops/crops.js
+++ src/const/crops/crops.js
@@ -1,8 +1,13 @@
+import carrot from '../../assets/crops/carrot.svg'
+import corn from '../../assets/crops/corn.svg'
+import wheat from '../../assets/crops/wheat.svg'
+
 export const WHEAT = {
   fieldName: 'wheat',
   count: 1,
   time: 2,
   cost: 4,
+  image: wheat,
 }
 
 export const CARROT = {
@@ -10,13 +15,15 @@ export const CARROT = {
   count: 1,
   time: 4,
   cost: 8,
+  image: carrot,
 }
 
-export const RICE = {
-  fieldName: 'rice',
+export const CORN = {
+  fieldName: 'corn',
   count: 2,
   time: 8,
   cost: 20,
+  image: corn,
 }
 
-export const list = [WHEAT, CARROT, RICE]
+export const list = [WHEAT, CARROT, CORN]
