bc1eaadecbbc4f544aef457f345100bebc801be4
Ihor Sharov <cookie50ds@gmail.com>
2023-12-03 00:15:22 +0200

    add harvest animation

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -2,54 +2,21 @@ import React from 'react';
 import PlantedField from './PlantedField';
 import EmptyField from './EmptyField';
 import CropSelector from '../crop-selector/CropSelector';
-import { motion } from 'framer-motion';
 import styles from './field.module.css';
+import { AnimatePresence } from 'framer-motion';
 
 export default function Field({ field }) {
   // TODO: Add field texture
-  const variants = {
-    initial: {
-      opacity: 0,
-
-      y: 200,
-      backdropFilter: 'blur(16px)',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-      transition: {
-        duration: 0,
-      },
-    },
-    animate: initial => ({
-      opacity: 1,
-      y: 0,
-      scale: 1,
-      rotate: 0,
-
-      backdropFilter: 'blur(0)',
-      transition: {
-        type: 'spring',
-        stiffness: 20,
-        ease: 'easyInOut',
-      },
-    }),
-    scale: {
-      scale: 1.1,
-      transition: {
-        ease: 'easeInOut',
-        delay: 0,
-      },
-    },
-  };
-
   return (
-    <motion.div
-      initial="initial"
-      animate="animate"
-      variants={variants}
-      className={styles.container}
-      whileHover="scale"
-    >
-      {field.plant ? <PlantedField {...field} /> : <EmptyField id={field.id} />}
+    <div className={styles.container}>
+      <AnimatePresence mode="wait">
+        {field.plant ? (
+          <PlantedField key={field.id + 'planted-field'} {...field} />
+        ) : (
+          <EmptyField id={field.id} />
+        )}
+      </AnimatePresence>
       <CropSelector id={field.id} />
-    </motion.div>
+    </div>
   );
 }
--- src/components/Field/PlantedField.jsx
+++ src/components/Field/PlantedField.jsx
@@ -4,7 +4,7 @@ import { storageSlice } from '../../store/slices/storage';
 import { areaSlice } from '../../store/slices/area';
 import styles from './field.module.css';
 import Timer from '../timer/Timer';
-import { motion, useAnimate } from 'framer-motion';
+import { easeInOut, easeOut, motion, useAnimate } from 'framer-motion';
 
 export default function PlantedField({ id, plant }) {
   const dispatch = useDispatch();
@@ -15,16 +15,11 @@ export default function PlantedField({ id, plant }) {
 
   function handleHarvest() {
     if (canHarvest.current) {
-      // TODO: create harvest animation
-      putToStorage();
+      dispatch(append(plant));
+      return dispatch(harvest(id));
     }
 
     handleEarlyHarvestAttempt();
-  }
-
-  function putToStorage(e) {
-    dispatch(append(plant));
-    return dispatch(harvest(id));
   }
 
   function growthEnd() {
@@ -48,13 +43,34 @@ export default function PlantedField({ id, plant }) {
     </motion.div>
   );
 }
+
+// TODO: separate to another file
 function GardenCup({ plant }) {
   return (
     <div className={styles.plantation}>
-      <img draggable={false} src={plant.image} alt={plant.fieldName} />
-      <img draggable={false} src={plant.image} alt={plant.fieldName} />
-      <img draggable={false} src={plant.image} alt={plant.fieldName} />
-      <img draggable={false} src={plant.image} alt={plant.fieldName} />
+      <CropImage plant={plant} />
+      <CropImage plant={plant} />
+      <CropImage plant={plant} />
+      <CropImage plant={plant} />
     </div>
+  );
+}
+
+// TODO: separate to another file
+function CropImage({ plant }) {
+  return (
+    <motion.img
+      initial={{ scale: 0.1 }}
+      animate={{ scale: 1 }}
+      exit={{
+        opacity: [1, 1, 0],
+        y: [0, -15, 20],
+        transition: { duration: 1, ease: easeInOut },
+      }}
+      draggable={false}
+      src={plant.image}
+      alt={plant.fieldName}
+      transition={{ duration: plant.time, ease: easeOut }}
+    />
   );
 }
