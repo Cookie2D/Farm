a7a815b870767f5e8b28bcbdbaa21094d8bd48db
Ihor Sharov <cookie50ds@gmail.com>
2023-12-02 13:53:00 +0200

    small fixes

--- src/components/Field/PlantedField.jsx
+++ src/components/Field/PlantedField.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { storageSlice } from '../../store/slices/storage';
 import { areaSlice } from '../../store/slices/area';
 import styles from './field.module.css';
