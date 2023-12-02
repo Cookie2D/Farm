fda8ec4835f744edd868e18cde0ffe3d197471f1
Ihor Sharov <cookie50ds@gmail.com>
2023-12-02 02:18:32 +0200

    add action sending in timer end

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import { normalizeTime } from '../../utils/normalizeTime';
+import { useDispatch } from 'react-redux';
 
-export default function Timer({ time }) {
+export default function Timer({ time, action }) {
   const [renderTime, setRenderTime] = React.useState(time);
+  const dispatch = useDispatch();
+  const isMounted = React.useRef(true);
 
   React.useEffect(() => {
     const id = setInterval(() => {
       setRenderTime(prev => {
         if (prev <= 0) {
           clearInterval(id);
+
+          if (isMounted.current) {
+            dispatch(action);
+          }
+
           return 0;
         }
         return prev - 1;
       });
     }, 1000);
 
-    return () => clearInterval(id);
-  }, []);
+    return () => {
+      clearInterval(id);
+      isMounted.current = false;
+    };
+  }, [dispatch, action]);
 
   return <div>{normalizeTime(renderTime)}s</div>;
 }
